70b4b7c0494fc7deaa4d2993ab2e76372c1f6b39
Matti Varjokallio <matti.varjokallio@iki.fi>
2014-01-23 21:44:39 +0200

    Script to print dot to pdf.

---
+++ dotpdf.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+import subprocess
+import sys
+import os
+
+
+if len(sys.argv) != 2:
+    print >>sys.stderr, "give .dot file as an argument"
+    sys.exit()
+
+dotname = sys.argv[1]
+(basename, ext) = os.path.splitext(dotname)
+epsname = basename + ".eps"
+p = subprocess.Popen("dot -Teps -Gcharset=latin1 %s > %s" % (dotname, epsname), shell=True)
+p.wait()
+p = subprocess.Popen("epspdf %s" % epsname, shell=True)
+p.wait()
+os.remove(epsname)
+
+pdfname = basename + ".pdf"
+subprocess.Popen("okular %s" % pdfname, shell=True)
+
