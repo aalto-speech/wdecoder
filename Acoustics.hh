481a9a283ab2e473855f47ac539817b4f7cbb72f
Matti Varjokallio <matti.varjokallio@iki.fi>
2014-01-29 16:38:35 +0200

    astyle.

--- Acoustics.hh
+++ Acoustics.hh
@@ -6,22 +6,26 @@
 
 class Acoustics {
 public:
-  inline Acoustics() : m_log_prob(NULL), m_num_models(0) { }
-  virtual ~Acoustics() { }
+    inline Acoustics() : m_log_prob(NULL), m_num_models(0) { }
+    virtual ~Acoustics() { }
 
-  /** Go to specified frame.  Returns false if frame is past end of file.
-   *
-   * Note that some derived classes preserve only a fixed amount of
-   * previous frames, because they support pipes.  Exception is thrown
-   * in that case.
-   **/
-  virtual bool go_to(int frame) = 0;
+    /** Go to specified frame.  Returns false if frame is past end of file.
+     *
+     * Note that some derived classes preserve only a fixed amount of
+     * previous frames, because they support pipes.  Exception is thrown
+     * in that case.
+     **/
+    virtual bool go_to(int frame) = 0;
 
-  inline float log_prob(int model) const { return m_log_prob[model]; }
-  inline int num_models() const { return m_num_models; }
+    inline float log_prob(int model) const {
+        return m_log_prob[model];
+    }
+    inline int num_models() const {
+        return m_num_models;
+    }
 protected:
-  float *m_log_prob;
-  int m_num_models;
+    float *m_log_prob;
+    int m_num_models;
 };
 
 #endif /* ACOUSTICS_HH */
--- LnaReaderCircular.hh
+++ LnaReaderCircular.hh
@@ -25,60 +25,60 @@
 
 class LnaReaderCircular : public Acoustics {
 public:
-  LnaReaderCircular();
-  inline void open_file(const char *filename, int buf_size) {
+    LnaReaderCircular();
+    inline void open_file(const char *filename, int buf_size) {
 
 #ifdef _MSC_VER
-  int fd = _open(filename, _O_RDONLY|_O_BINARY);
+        int fd = _open(filename, _O_RDONLY|_O_BINARY);
 #else
-  int fd = open(filename, O_RDONLY|O_BINARY);
+        int fd = open(filename, O_RDONLY|O_BINARY);
 #endif
 
-  if (fd < 0) {
-      fprintf(stderr, "LnaReaderCircular::open(): could not open %s: %s\n",
-	      filename, strerror(errno));
-      exit(1);
+        if (fd < 0) {
+            fprintf(stderr, "LnaReaderCircular::open(): could not open %s: %s\n",
+                    filename, strerror(errno));
+            exit(1);
+        }
+        open_fd(fd, buf_size);
     }
-    open_fd(fd, buf_size);
-  }
-  void open_fd(const int fd, int buf_size);
-  void close();
-  void seek(int frame);
-  
-  virtual bool go_to(int frame);
+    void open_fd(const int fd, int buf_size);
+    void close();
+    void seek(int frame);
+
+    virtual bool go_to(int frame);
 
 private:
-  int read_int();
+    int read_int();
 
-  FILE *m_file;
+    FILE *m_file;
 
-  // INVARIANTS
-  //
-  // - m_frames_read contains the number of read frames (from the
-  // beginning of the file)
-  //
-  // - m_first_index'th position in m_buffer contains the first value
-  // of the earliest frame (also the first value of the frame which is
-  // read next)
-  //
-  // - m_first_index'th position in m_log_prob_buffer contains frame
-  // (m_frames - m_log_prob_buffer.size())
-  //
-  // - (m_index-m_num_models)'th position in m_buffer contains frame 
-  // (m_frames - 1)
+    // INVARIANTS
+    //
+    // - m_frames_read contains the number of read frames (from the
+    // beginning of the file)
+    //
+    // - m_first_index'th position in m_buffer contains the first value
+    // of the earliest frame (also the first value of the frame which is
+    // read next)
+    //
+    // - m_first_index'th position in m_log_prob_buffer contains frame
+    // (m_frames - m_log_prob_buffer.size())
+    //
+    // - (m_index-m_num_models)'th position in m_buffer contains frame
+    // (m_frames - 1)
 
-  int m_header_size;  // The size of file header in bytes.
-  int m_buffer_size;  // Size of buffer in frames 
-  int m_first_index;  // Index of the last value in the last frame
-  int m_frames_read;  // Frames read
-  std::vector<float> m_log_prob_buffer;
-  int m_eof_frame;
-  int m_frame_size;
+    int m_header_size;  // The size of file header in bytes.
+    int m_buffer_size;  // Size of buffer in frames
+    int m_first_index;  // Index of the last value in the last frame
+    int m_frames_read;  // Frames read
+    std::vector<float> m_log_prob_buffer;
+    int m_eof_frame;
+    int m_frame_size;
 
-  // Temporary buffers
-  std::vector<char> m_read_buffer;
+    // Temporary buffers
+    std::vector<char> m_read_buffer;
 
-  int m_lna_bytes;
+    int m_lna_bytes;
 };
 
 #endif /* LNAREADERCIRCULAR_HH */
