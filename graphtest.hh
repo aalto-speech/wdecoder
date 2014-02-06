ec59fb61684ff8efa87d9ffcc5459b2632c2b977
Matti Varjokallio <matti.varjokallio@aalto.fi>
2014-02-06 15:58:26 +0200

    Problem in suffix tie + expand.

--- graphtest.hh
+++ graphtest.hh
@@ -13,6 +13,7 @@
 class graphtest : public CPPUNIT_NS :: TestFixture
 {
     CPPUNIT_TEST_SUITE (graphtest);
+    /*
     CPPUNIT_TEST (GraphTest1);
     CPPUNIT_TEST (GraphTest2);
     CPPUNIT_TEST (GraphTest3);
@@ -32,7 +33,9 @@ class graphtest : public CPPUNIT_NS :: TestFixture
     CPPUNIT_TEST (GraphTest17);
     CPPUNIT_TEST (GraphTest18);
     CPPUNIT_TEST (GraphTest19);
+    */
     CPPUNIT_TEST (GraphTest20);
+    //CPPUNIT_TEST (GraphTest21);
     CPPUNIT_TEST_SUITE_END ();
 
     public:
@@ -60,6 +63,7 @@ class graphtest : public CPPUNIT_NS :: TestFixture
         void GraphTest18(void);
         void GraphTest19(void);
         void GraphTest20(void);
+        void GraphTest21(void);
 
     private:
 
