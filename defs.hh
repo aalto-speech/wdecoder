3cafe3e77605be9f8bc6227eaeff870173dc2297
Matti Varjokallio <matti.varjokallio@iki.fi>
2014-02-14 12:05:32 +0200

    Removed and added some files.

---
+++ defs.hh
@@ -0,0 +1,11 @@
+#ifndef DEFS_HH
+#define DEFS_HH
+
+#define START_NODE   0
+#define END_NODE     1
+
+#define NODE_FAN_OUT_DUMMY              0x01
+#define NODE_FAN_IN_DUMMY               0x02
+#define NODE_CW                         0x04
+
+#endif /* DEFS_HH */
