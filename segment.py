5e7b121ce26a168b37bcb67e8200514f3af88ea7
Matti Varjokallio <matti.varjokallio@aalto.fi>
2015-02-03 13:23:27 +0200

    Added script for segmenting.

---
+++ segment.py
@@ -0,0 +1,74 @@
+#!/usr/bin/python
+
+import os
+import sys
+import glob
+import tempfile
+import threading
+import subprocess
+from optparse import OptionParser
+
+
+def segment(model, recipe, lnadir, batches, bidx):
+
+    work_dir = os.path.join(lnadir, "segment_temp_%i" % bidx)
+    #if not os.path.exists(work_dir): os.makedirs(work_dir)
+
+    #pp_exe = 'phone_probs'
+    #pp_cmd = "%s -b %s -c %s -r %s -o %s -C %s --eval-ming=0.15 -B %i -I %i" %\
+    #         (pp_exe, model, "%s.cfg" % model, recipe, lnadir, "%s.gcl" % model, batches, bidx)
+    #p = subprocess.Popen(pp_cmd, shell=True)
+    #p.wait()
+
+    segment_exe = '/home/mavarjok/wdecoder/segment'
+    model_ph = "%s.ph" % model
+    model_dur = "%s.dur" % model
+    batch_options = "-B %i -I %i" % (batches, bidx)
+    seg_cmd = "%s -t -l -s -d %s %s %s %s" % (segment_exe, model_dur, batch_options, model_ph, recipe)
+    p = subprocess.Popen(seg_cmd, shell=True)
+    p.wait()
+
+
+def main(argv=None):
+
+    usage = "usage: %prog [options] MODEL RECIPE BATCHES"
+    parser = OptionParser(usage=usage)
+
+    (options, args) = parser.parse_args()
+    if len(args) != 3:
+        parser.print_help()
+        return
+
+    model = args[0]
+    recipe = args[1]
+    batches = int(args[2])
+
+    # amodel_suffixes = ['ph', 'mc', 'gk', 'cfg', 'gcl']
+    # for suffix in amodel_suffixes:
+    #     amfname = "%s.%s" % (model, suffix)
+    #     if not os.path.exists(amfname):
+    #         print >>sys.stderr, "%s not found" % amfname
+    #         sys.exit()
+    #
+    # if not os.path.exists(recipe):
+    #     print >>sys.stderr, "recipe %s not found" % recipe
+    #     sys.exit()
+    #
+
+    lnadir = tempfile.gettempdir()
+    if not lnadir:
+        print >>sys.stderr, "Could not find temporary directory for lnas"
+        sys.exit()
+
+    threads = []
+    for i in range(batches):
+        bidx = i+1
+        t = threading.Thread(target=segment, args=[model, recipe, lnadir, batches, bidx])
+        t.start()
+        #threads.append(t)
+        t.join()
+    #for t in threads: t.join()
+
+
+if __name__ == "__main__":
+    sys.exit(main())
