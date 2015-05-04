f3a92cf154487c4ffb2b372d7ef83e08aa554568
Matti Varjokallio <matti.varjokallio@aalto.fi>
2015-05-04 17:51:25 +0300

    Fix.

--- swgraph.cc
+++ swgraph.cc
@@ -14,32 +14,31 @@ int main(int argc, char* argv[])
     if (config.arguments.size() != 3) config.print_help(stderr, 1);
     bool no_start_end_wb = config["no-start-end-wb"].specified;
 
-    DecoderGraph dg;
+    SubwordGraph swg;
 
     try {
         string phfname = config.arguments[0];
         cerr << "Reading hmms: " << phfname << endl;
-        dg.read_phone_model(phfname);
+        swg.read_phone_model(phfname);
 
         string lexfname = config.arguments[1];
         cerr << "Reading lexicon: " << lexfname << endl;
-        dg.read_noway_lexicon(lexfname);
+        swg.read_noway_lexicon(lexfname);
 
         string graphfname = config.arguments[2];
         cerr << "Result graph file name: " << graphfname << endl;
 
         set<string> subwords;
-        for (auto swit = dg.m_lexicon.begin(); swit != dg.m_lexicon.end(); ++swit)
+        for (auto swit = swg.m_lexicon.begin(); swit != swg.m_lexicon.end(); ++swit)
             subwords.insert(swit->first);
 
-        SubwordGraph swg;
         swg.create_graph(subwords, true);
 
         if (no_start_end_wb)
             swg.add_long_silence_no_start_end_wb();
         else {
             swg.add_long_silence();
-            swg.m_nodes[END_NODE].word_id = dg.m_subword_map["<w>"];
+            swg.m_nodes[END_NODE].word_id = swg.m_subword_map["<w>"];
         }
 
         swg.add_hmm_self_transitions();
