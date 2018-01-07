552ea661cba42c0b1fa81cd4f4fc39b267e4a1d9
Matti Varjokallio <matti.varjokallio@aalto.fi>
2018-01-07 22:35:28 +0200

    Working dynamic bitset.

--- util/DynamicBitset.cc
+++ util/DynamicBitset.cc
@@ -1,24 +1,41 @@
+#include <climits>
+#include <string>
+#include <iostream>
+
+#include "defs.hh"
 #include "DynamicBitset.hh"
+
+#define BITS_PER_CHAR 8
+
+using namespace std;
 
 DynamicBitset::DynamicBitset(
     int numBits,
     bool initValue)
     : numBits(numBits)
 {
-    data.resize(1);
-    for (int i=0; i<numBits; i++)
-        setBit(i, initValue);
+    data.resize((numBits / BITS_PER_CHAR) + 1, initValue ? UCHAR_MAX : 0);
 }
 
 bool
 DynamicBitset::getBit(int idx)
 {
-    return true;
+    if (idx >= numBits)
+        throw string("trying to get bit " + int2str(idx)
+            + ", but the total number of bits is " + int2str(numBits));
+    unsigned int byteIdx = idx / BITS_PER_CHAR;
+    unsigned char bitMask = 1 << (idx % BITS_PER_CHAR);
+    return data[byteIdx] & bitMask;
 }
 
 void
 DynamicBitset::setBit(int idx, bool bitValue)
 {
-    data[0] = bitValue;
+    if (idx >= numBits)
+        throw string("trying to set bit " + int2str(idx)
+            + ", but the total number of bits is " + int2str(numBits));
+    unsigned int byteIdx = idx / BITS_PER_CHAR;
+    unsigned char bitMask = 1 << (idx % BITS_PER_CHAR);
+    if ((data[byteIdx] & bitMask) != bitValue)
+        data[byteIdx] ^= bitMask;
 }
-
